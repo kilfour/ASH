ac34c7ad3fee9afd05311d8f77cb97edd4b572ef
Bennyam <ben.ameryckx@icloud.com>
2025-06-02 11:14:34 +0200

    Story 1 en 2 afgewerkt

--- observation-deck/src/black-box/index.js
+++ observation-deck/src/black-box/index.js
@@ -1,12 +1,56 @@
-export function blackBoxFunction() {
-    return true;
+import {
+  journals,
+  addJournal,
+  deleteJournal,
+  searchJournal,
+  editJournal,
+} from "./modules/journalsState.js";
+
+const formEl = document.querySelector(".journal");
+
+function displayJournals(journals) {
+  const journalsView = document.querySelector(".journals");
+  journalsView.innerHTML = "";
+
+  journals.forEach(function ({ id, titel, content, tags }) {
+    const html = `
+        <div class=journal id=${id}>
+          <h2 class="titel">${titel}</h2>
+          <p class="content">${content}<p>
+          <p class="tags">${tags.join(", ")}<p>`;
+
+    journalsView.insertAdjacentHTML("afterbegin", html);
+  });
 }
 
-
-
-export function searchContent(str, searchstr){
-    return str.split(' ').some(word => word.toLowerCase().includes(searchstr.toLowerCase()));    
+function upDateUi() {
+  displayJournals(journals);
 }
+upDateUi();
+
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault("");
+
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData);
+
+  const newJournal = {
+    ...data,
+    id: "123455667",
+    tags: [data.tags.split(",")],
+  };
+
+  addJournal(newJournal);
+  upDateUi();
+});
+
+// export function blackBoxFunction() {
+//     return true;
+// }
+
+// export function searchContent(str, searchstr){
+//     return str.split(' ').some(word => word.toLowerCase().includes(searchstr.toLowerCase()));
+// }
 //The .some() method in JavaScript is used on arrays to check if at least one element satisfies a given condition.
 //It returns a boolean
 
@@ -23,5 +67,3 @@ function searchContent(str, searchstr){
     return result;
 }
 */
-
-
