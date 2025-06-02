3785751e719a742e18a48ecda77f1c8b96fac987
kilfour <kilfour@gmail.com>
2025-06-02 20:36:07 +0200

    removed unused files

--- observation-deck/tests/@aperture-core/util.test.js
+++
@@ -1,6 +0,0 @@
-import { describe, it, expect } from 'vitest';
-import { utilFunction } from '../../src/@aperture-core/util';
-
-it('aperture can be tested', () => {
-    expect(utilFunction()).toBe(true);
-});
