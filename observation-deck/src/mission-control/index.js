0b043c36cde806547e3c33bc379240f17760df6e
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-06-02 14:11:23 +0200

    save button added

--- observation-deck/src/mission-control/index.js
+++ observation-deck/src/mission-control/index.js
@@ -1,3 +1,43 @@
-export function missionControl() {
-    return true;
-}
+document.getElementById("testbtn").addEventListener("click", () => console.log('test'))
+
+
+const addStoryBtn = document.getElementById('create-story-button');
+const overlay = document.getElementById('popup-overlay');
+const closeStoryBtn = document.getElementById('close-popup');
+const saveStoryBtn = document.getElementById('save-story');
+
+addStoryBtn.addEventListener('click', () => {
+    overlay.classList.remove('hidden');
+});
+closeStoryBtn.addEventListener('click', () => {
+    document.getElementById('modal-confirm').classList.remove('hidden');
+});
+
+document.getElementById('modal-yes').onclick = () => {
+    overlay.classList.add('hidden');
+    document.getElementById('modal-confirm').classList.add('hidden');
+};
+
+document.getElementById('modal-no').onclick = () => {
+    document.getElementById('modal-confirm').classList.add('hidden');
+};
+
+const stories = [];
+saveStoryBtn.addEventListener('click', () => {
+    const title = document.getElementById('story-title-name').value.trim(); // deleting spaces in start and end of string avoiding
+    const description = document.getElementById('story-description').value.trim();
+
+    if (title && description) {
+        const story = {
+            title: title,
+            description: description
+        };
+        stories.push(story);
+        console.log(story);
+        console.log(stories);
+        overlay.classList.add('hidden');
+        document.getElementById('story-title-name').value = '';
+        document.getElementById('story-description').value = '';
+    }
+})
+
