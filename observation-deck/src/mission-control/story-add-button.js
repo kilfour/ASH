54cf986b69526343e50626707f86c0375ba3e842
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-06-02 16:50:22 +0200

    refactor addButton code

--- observation-deck/src/mission-control/story-add-button.js
+++ observation-deck/src/mission-control/story-add-button.js
@@ -1,5 +1,14 @@
 import { storeUserStory } from "./localStorage.js"
 
+const el = {
+    addBTN: document.getElementById('create-story-button'),
+    overlay: document.getElementById('popup-overlay'),
+    closeBTN: document.getElementById('close-popup'),
+    saveBTN: document.getElementById('save-story'),
+    title: document.getElementById('story-title-name'),
+    desc: document.getElementById('story-description'),
+    modal: document.getElementById('modal-confirm'),
+}
 
 const addStoryBtn = document.getElementById('create-story-button');
 const overlay = document.getElementById('popup-overlay');
