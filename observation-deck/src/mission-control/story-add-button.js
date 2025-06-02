d9e9620067ac3090e446a0c11d551f743f486196
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-06-02 19:00:26 +0200

    Add Button refactoring done

--- observation-deck/src/mission-control/story-add-button.js
+++ observation-deck/src/mission-control/story-add-button.js
@@ -1,5 +1,5 @@
 import { storeUserStory } from "./localStorage.js"
-//  Alx:: TODO
+
 const el = {
     addBTN: document.getElementById('create-story-button'),
     overlay: document.getElementById('popup-overlay'),
@@ -8,49 +8,45 @@ const el = {
     title: document.getElementById('story-title-name'),
     desc: document.getElementById('story-description'),
     modal: document.getElementById('modal-confirm'),
-}
-
-
-const addStoryBtn = document.getElementById('create-story-button');
-const overlay = document.getElementById('popup-overlay');
-const closeStoryBtn = document.getElementById('close-popup');
-const saveStoryBtn = document.getElementById('save-story');
-const storyTitleName = document.getElementById('story-title-name')
-const storyDescription = document.getElementById('story-description');
+};
 
 function toggleElements(elements, action) {
     elements.forEach(el => el.classList[action]('hidden'));
-}
+};
 
-addStoryBtn.addEventListener('click', () => {
-    overlay.classList.remove('hidden');
+function resetStory() {
+    el.title.value = '';
+    el.desc.value = '';
+};
+
+el.addBTN.addEventListener('click', () => {
+    el.overlay.classList.remove('hidden');
 });
 
-closeStoryBtn.addEventListener('click', () => {
-    document.getElementById('modal-confirm').classList.remove('hidden');
+el.closeBTN.addEventListener('click', () => {
+    el.modal.classList.remove('hidden');
     toggleElements([
-        closeStoryBtn, saveStoryBtn, storyDescription, storyTitleName], 'add')
+        el.closeBTN, el.saveBTN, el.desc, el.title], 'add')
 });
 
 document.getElementById('modal-yes').onclick = () => {
-    overlay.classList.add('hidden');
-    document.getElementById('modal-confirm').classList.add('hidden');
-    document.getElementById('story-title-name').value = '';
-    document.getElementById('story-description').value = '';
+    el.overlay.classList.add('hidden');
+    el.modal.classList.add('hidden');
+    resetStory();
 
     toggleElements(
-        [closeStoryBtn, saveStoryBtn, storyDescription, storyTitleName],
+        [el.closeBTN, el.saveBTN, el.desc, el.title],
          'remove')
 };
 
 document.getElementById('modal-no').onclick = () => {
-    document.getElementById('modal-confirm').classList.add('hidden');
-    toggleElements([closeStoryBtn, saveStoryBtn, storyDescription, storyTitleName], 'remove')
+    el.modal.classList.add('hidden');
+    toggleElements([el.closeBTN, el.saveBTN, el.desc, el.title], 'remove')
 };
 
-saveStoryBtn.addEventListener('click', () => {
-    const title = document.getElementById('story-title-name').value.trim(); // deleting spaces in start and end of string avoiding
-    const description = document.getElementById('story-description').value.trim();
+el.saveBTN.addEventListener('click', () => {
+    const title = el.title.value.trim(); // .trim() is deleting spaces in start and end of string avoiding
+    const description = el.desc.value.trim();
 
     if (title && description) {
         const story = {
@@ -59,9 +55,8 @@ saveStoryBtn.addEventListener('click', () => {
         };
   
         storeUserStory(story);
-        overlay.classList.add('hidden');
-        document.getElementById('story-title-name').value = '';
-        document.getElementById('story-description').value = '';
+        el.overlay.classList.add('hidden');
+        resetStory();
     }
-})
+});
 
