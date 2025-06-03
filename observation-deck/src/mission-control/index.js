50f74f31fe0f2a7198d1348b2ff092d48cd5ca95
free2c0ma <75183012+saqniok@users.noreply.github.com>
2025-06-03 10:27:58 +0200

    fixing button story

--- observation-deck/src/mission-control/index.js
+++ observation-deck/src/mission-control/index.js
@@ -1,8 +1,8 @@
-import { storeUserStory } from "./Storage/localStorage"
+import { storeUserStory } from "./Storage/localStorage.js"
 /**
  * Object with popup elements 
  */
-const el = { // MME: el could use a better name, 'elements' for instance
+const elements = {
     addBTN: document.getElementById('create-story-button'),
     overlay: document.getElementById('popup-overlay'),
     closeBTN: document.getElementById('close-popup'),
@@ -17,22 +17,27 @@ const el = { // MME: el could use a better name, 'elements' for instance
 /**
  *  Functions to toggle elements in the DOM
  */
-function toggleElements(elements, action) {
-    elements.forEach(el => el.classList[action]('hidden'));
+function toggleElements(element, action) {
+    element.forEach(el => el.classList[action]('hidden'));
 };
 
 function resetStory() {
-    el.title.value = '';
-    el.desc.value = '';
+    elements.title.value = '';
+    elements.desc.value = '';
 };
 
-el.addBTN.addEventListener('click', () => {
-    el.overlay.classList.remove('hidden');
+// function hide(el, action) {
+//   if (action === 'hide') { el.classList.add('hidden'); }
+//   else if (action === 'add') { el.classList.remove('hidden'); }
+// };
+
+elements.addBTN.addEventListener('click', () => {
+    elements.overlay.classList.remove('hidden');
 });
 
-el.closeBTN.addEventListener('click', () => {
-    el.modal.classList.remove('hidden');
-    toggleElements([el.closeBTN, el.saveBTN, el.desc, el.title], 'add')
+elements.closeBTN.addEventListener('click', () => {
+    elements.modal.classList.remove('hidden');
+    toggleElements([elements.closeBTN, elements.saveBTN, elements.desc, elements.title], 'add')
 });
 
 // ------------------------------------------------------------------------
@@ -44,33 +49,28 @@ el.closeBTN.addEventListener('click', () => {
 //     el.overlay.classList.add('hidden');
 //     ...
 // }
-el.modalYes.onclick = () => {
-<<<<<<< HEAD:observation-deck/src/mission-control/story-add-button.js
-    toggleElements([el.overlay, el.modal], 'add')
-    resetStory();
-=======
-    el.overlay.classList.add('hidden'); // MME: wrap this in a simple hide(element) function
-    el.modal.classList.add('hidden');   // 'cause you're using it all over the place
+elements.modalYes.onclick = () => {
+    elements.overlay.classList.add('hidden'); // MME: wrap this in a simple hide(element) function
+    elements.modal.classList.add('hidden');   // 'cause you're using it all over the place
     resetStory();                       // 
     // el.overlay.classList.add('hidden');
     //    => tells me how it works (i only need to see this once)
     // hide(el.overlay)
     //    => tells me what it does, always usefull
->>>>>>> 529127842f379e1fc98e91a39aee49a4c3303f86:observation-deck/src/mission-control/index.js
 
     toggleElements(
-        [el.closeBTN, el.saveBTN, el.desc, el.title],
+        [elements.closeBTN, elements.saveBTN, elements.desc, elements.title],
         'remove')
 };
 
-el.modalNo.onclick = () => {
-    el.modal.classList.add('hidden');
-    toggleElements([el.closeBTN, el.saveBTN, el.desc, el.title], 'remove')
+elements.modalNo.onclick = () => {
+    elements.modal.classList.add('hidden');
+    toggleElements([elements.closeBTN, elements.saveBTN, elements.desc, elements.title], 'remove')
 };
 
-el.saveBTN.addEventListener('click', () => {
-    const title = el.title.value.trim(); // .trim() removes spaces at the beginning and end of a line, avoiding them
-    const description = el.desc.value.trim();
+elements.saveBTN.addEventListener('click', () => {
+    const title = elements.title.value.trim(); // .trim() removes spaces at the beginning and end of a line, avoiding them
+    const description = elements.desc.value.trim();
 
     if (title && description) {
         const story = {               // MME: move creating a story, validation and above trim etc, 
@@ -79,7 +79,7 @@ el.saveBTN.addEventListener('click', () => {
         };
 
         storeUserStory(story);
-        el.overlay.classList.add('hidden');
+        elements.overlay.classList.add('hidden');
         resetStory();
     }
 });
