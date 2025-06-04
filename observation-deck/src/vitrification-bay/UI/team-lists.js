7be9924676e9612677ccebf986a0577a05a9646c
kilfour <kilfour@gmail.com>
2025-06-04 08:44:46 +0200

    defining a problem

--- observation-deck/src/vitrification-bay/UI/team-lists.js
+++ observation-deck/src/vitrification-bay/UI/team-lists.js
@@ -1,15 +1,18 @@
-import { html, htmlList } from "../_utils/fabrication_facility";
+import { html, htmlList } from '../_utils/fabrication_facility';
 
 const teamOrange = ['Laurens', 'Benny', 'Mathias', 'Michael'];
 const teamBlue = ['Alex', 'Milad', 'Abigail', 'Naomi'];
 
 const container =
-    html("div",
-        html("h2", "🔸Team Orange"),
-        htmlList("ul", teamOrange, name => html("li", name)),
-        html("h2", "🔹Team Blue"),
-        htmlList("ul", teamBlue, name => html("li", name))
-    );
+    html('div',
+        html('div',
+            html('div',
+                html('h2', '🔸Team Orange'),
+                htmlList('ul', teamOrange, name => html('li', name))),
+            html('div',
+                html('h2', '🔹Team Blue'),
+                htmlList('ul', teamBlue, name => html('li', name)))
+        ));
 
 export function renderTeamLists(parent) {
     parent.appendChild(container);
