94002ea6ab6cfb75de81655640577e81503572fb
NaomiND <naomidhooghe@hotmail.com>
2025-06-05 11:12:12 +0200

    filter on main page

--- observation-deck/src/mission-control/index.js
+++ observation-deck/src/mission-control/index.js
@@ -3,7 +3,7 @@ import { getUserStory } from "./Entities/userStory.js";
 import elements, { formStory } from "./UI/UserStoryForm/domElements.js"
 import { show, hide, toggleElements} from "./UI/UserStoryForm/uiHelpers.js"
 import { onAddButtonClick, onCloseButtonClick, onModalYesClick, onModalNoClick, resetStory }from "./UI/UserStoryForm/eventHandelers.js"
-
+import {onOpenFilterClicked} from "./UI/UserStoryList/filter.js"
 
 
 function onSaveButtonClick() {
@@ -100,3 +100,5 @@ elements.closeBTN.onclick = onCloseButtonClick;
 elements.modalYes.onclick = onModalYesClick;
 elements.modalNo.onclick = onModalNoClick;
 elements.saveBTN.onclick = onSaveButtonClick;
+
+document.getElementById("open-filter").addEventListener('click', onOpenFilterClicked)
